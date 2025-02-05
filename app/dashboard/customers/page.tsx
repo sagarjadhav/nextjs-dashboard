9318aebe7a861bb42a0ff7f7c010fc104fbd01f6
Sagar Jadhav <sagarjadhav27@gmail.com>
2025-02-05 12:15:30 +0530

    Complete up to Chapter 5

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+// page.tsx
+// # Represents a specific page in Next.js.
+// # Contains the primary content for that route.
+// # Receives the layout from layout.tsx if it exists.
+
+export default function Page() {
+    return <p>Customers Page</p>;
+}
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+// layout.tsx
+// # Used for shared layouts across multiple pages.
+// # Wraps child components (including page.tsx).
+// # Typically includes UI elements like navigation bars, sidebars, and footers.
+// # Remains persistent across navigations, preventing unnecessary re-renders.
+
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
